dcf34f680a3e55b67234baae0b4290d18d9aa1e8
itsxilef <114369509+itsxilef@users.noreply.github.com>
2024-12-22 23:19:57 -0500

    just setting shit up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { TicTacToe } from './Components/TicTacToe/TicTacToe';
 
+
+
 function App() {
   return (
     <div>
--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,32 @@
 
 import React from 'react'
 import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
 
 export const TicTacToe = () => {
   return (
-    <div>TicTacToe</div>
+    <div className = 'container'>
+      <h1 className="title">Tic Tac Toe Game In <span>React</span></h1> 
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+    <button className="reset">Reset</button>
+
+    </div>
   )
 }
